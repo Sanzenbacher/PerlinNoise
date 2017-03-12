12cf7b62e0436b11707b7ab70266293a494ca03e
robicity <robicity@users.noreplay.github.com>
2017-03-12 17:30:40 +0100

    test

--- Win32PerlinNoise/build.bat
+++ Win32PerlinNoise/build.bat
@@ -1,5 +1,5 @@
 @echo off
-REM  Run vc Build Tools
+REM  Run vc Build Tools - test git
 call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" x64    
 set compilerflags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHsc- -Zo -Oi -WX -W4 -FC -Z7
 set linkerflags=/OUT:win64_perlinnoise.exe  -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib
